17a0f2d15ef9704ca663caaf18e3440342282e8b
Emma Pinegar <u1008834@utah.edu>
2024-09-09 14:43:03 -0600

    start to see if tracking the angle is causing the seg fault

--- include/impl/prrt/node.hpp
+++ include/impl/prrt/node.hpp
@@ -46,6 +46,7 @@ class Node {
     Edge<State, Traj> parent_;
     Scalar traj_length_{0};
     Scalar cost_{0};
+    Scalar ang_total_{0};
 
   public:
     template <typename ... Args>
@@ -69,6 +70,14 @@ class Node {
 
     const Scalar& cost() const {
         return cost_;
+    }
+
+    Scalar& ang_total() {
+        return ang_total_;
+    }
+
+    const Scalar& ang_total() const {
+        return ang_total_;
     }
 
     const State& state() const {
